3728c2269c16af65a62978adc74483e475f3438f
Vitalik@1992 <vitalii.rubezhanskii@gmail.com>
2018-10-30 13:29:07 +0200

    Initial commit

---
+++ src/main/java/ua/rubezhanskii/ticketapp/MongoDB/MongoConfig.java
@@ -0,0 +1,4 @@
+package ua.rubezhanskii.ticketapp.MongoDB;
+
+public class MongoConfig {
+}
---
+++ src/test/java/ua/rubezhanskii/ticketapp/TicketappApplicationTests.java
@@ -0,0 +1,16 @@
+package ua.rubezhanskii.ticketapp;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TicketappApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
